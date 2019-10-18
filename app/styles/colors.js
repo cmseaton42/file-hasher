3d2ad291d7dc1ef7fc6c4c8e6b2045dd2becd350
Canaan Seaton <cmseaton42@gmail.com>
2019-10-18 07:38:19 -0700

    Add basic styles

---
+++ app/styles/colors.js
@@ -0,0 +1,22 @@
+const colors = {
+    // Primary Company Color Pallet
+    PRIMARY: "#004A8D",
+    PRIMARY_SHADE: "#003362",
+    PRIMARY_TINT: "#0065c0",
+    SECONDARY: "#0078C1",
+    SECONDARY_TINT: "#0fa4ff",
+    SECONDARY_TINT_2: "#7DCDFF",
+
+    // Useful Colors
+    WHITE: "#F8F8FF",
+    OFF_WHITE: "#F8F8FF",
+    GREY: "#757575",
+    LIGHT_GREY: "#cacad0",
+    LIGHTER_GREY: "#E4E4E7",
+    RED: "#cc0000",
+    RED_TINT: "#ff0000",
+    GREEN: "#248f24",
+    GREEN_TINT: "#33cc33"
+};
+
+export default colors;
